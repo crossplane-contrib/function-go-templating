fd034d539ce4e5c85e33a27a6f909808bda298e8
Jan Dittrich <jan.dittrich@cgm.com>
2023-11-16 11:44:01 +0100

    Simplify function using fieldpath.Pave
    Signed-off-by: Jan Dittrich <jan.dittrich@cgm.com>

--- function_maps.go
+++ function_maps.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"encoding/json"
 	"math/rand"
 	"text/template"
 	"time"
 
 	"gopkg.in/yaml.v3"
-	v1 "k8s.io/api/core/v1"
 
 	sprig "github.com/Masterminds/sprig/v3"
 
 	xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
-	"github.com/crossplane/function-sdk-go/errors"
+	"github.com/crossplane/crossplane-runtime/pkg/fieldpath"
 )
 
 var funcMaps = []template.FuncMap{
@@ -37,37 +35,15 @@ var funcMaps = []template.FuncMap{
 			return res, err
 		},
 
-		"getResourceCondition": func(ct string, res map[string]any) (*xpv1.Condition, error) {
-			resource, ok := res["resource"]
-			if !ok {
-				return nil, errors.New("input is not a resource")
-			}
-
-			status, ok := resource.(map[string]any)["status"]
-			if !ok {
-				// Just return a unknown condition for resources that do not have a status (yet)
-				return &xpv1.Condition{
-					Type:   xpv1.ConditionType(ct),
-					Status: v1.ConditionUnknown,
-				}, nil
-			}
-
-			// Convert map to JSON string
-			reqJson, err := json.Marshal(status)
-			if err != nil {
-				return nil, errors.Wrap(err, "cannot marshal into json")
-			}
-
-			// Unmarshal JSON string into struct
+		"getResourceCondition": func(ct string, res map[string]any) (xpv1.Condition, error) {
 			var conditioned xpv1.ConditionedStatus
-			err = json.Unmarshal(reqJson, &conditioned)
-			if err != nil {
-				return nil, errors.Wrap(err, "cannot unmarshal into ConditionedStatus")
+			if err := fieldpath.Pave(res).GetValueInto("resource.status", &conditioned); err != nil {
+				conditioned = xpv1.ConditionedStatus{}
 			}
 
 			// Return either found condition or empty one with "Unknown" status
 			cond := conditioned.GetCondition(xpv1.ConditionType(ct))
-			return &cond, nil
+			return cond, nil
 		},
 	},
 }
