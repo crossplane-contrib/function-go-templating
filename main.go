7432d3e4938c0862dd5a43a89e04a914e9d99ac8
Steven Borrelli <steve@borrelli.org>
2025-04-17 18:07:55 +0200

    make max-recv-message-size a cli option
    Signed-off-by: Steven Borrelli <steve@borrelli.org>

--- main.go
+++ main.go
@@ -7,17 +7,15 @@ import (
 	"github.com/crossplane/function-sdk-go"
 )
 
-// MaxRecvMessageSize sets the maximum message size. gRPC default is 4MB
-const MaxRecvMessageSize = 1024 * 1024 * 16
-
 // CLI of this Function.
 type CLI struct {
 	Debug bool `short:"d" help:"Emit debug logs in addition to info logs."`
 
-	Network     string `help:"Network on which to listen for gRPC connections." default:"tcp"`
-	Address     string `help:"Address at which to listen for gRPC connections." default:":9443"`
-	TLSCertsDir string `help:"Directory containing server certs (tls.key, tls.crt) and the CA used to verify client certificates (ca.crt)" env:"TLS_SERVER_CERTS_DIR"`
-	Insecure    bool   `help:"Run without mTLS credentials. If you supply this flag --tls-server-certs-dir will be ignored."`
+	Network            string `help:"Network on which to listen for gRPC connections." default:"tcp"`
+	Address            string `help:"Address at which to listen for gRPC connections." default:":9443"`
+	TLSCertsDir        string `help:"Directory containing server certs (tls.key, tls.crt) and the CA used to verify client certificates (ca.crt)" env:"TLS_SERVER_CERTS_DIR"`
+	Insecure           bool   `help:"Run without mTLS credentials. If you supply this flag --tls-server-certs-dir will be ignored."`
+	MaxRecvMessageSize int    `help:"gRPC maximum message size in bytes. Increase this when processing large Composites." default:"4194304"`
 }
 
 // Run this Function.
@@ -35,8 +33,7 @@ func (c *CLI) Run() error {
 		function.Listen(c.Network, c.Address),
 		function.MTLSCertificates(c.TLSCertsDir),
 		function.Insecure(c.Insecure),
-		function.MaxRecvMessageSize(MaxRecvMessageSize),
-	)
+		function.MaxRecvMessageSize(c.MaxRecvMessageSize))
 }
 
 func main() {
