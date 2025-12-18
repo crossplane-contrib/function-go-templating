1db428e3f24796a929213fe2753bb517eacfc47c
Bob Haddleton <bob.haddleton@nokia.com>
2025-12-17 19:34:32 -0600

    Fix resulting lint errors
    Signed-off-by: Bob Haddleton <bob.haddleton@nokia.com>

--- main.go
+++ main.go
@@ -9,13 +9,13 @@ import (
 
 // CLI of this Function.
 type CLI struct {
-	Debug bool `short:"d" help:"Emit debug logs in addition to info logs."`
+	Debug bool `help:"Emit debug logs in addition to info logs." short:"d"`
 
-	Network            string `help:"Network on which to listen for gRPC connections." default:"tcp"`
-	Address            string `help:"Address at which to listen for gRPC connections." default:":9443"`
-	TLSCertsDir        string `help:"Directory containing server certs (tls.key, tls.crt) and the CA used to verify client certificates (ca.crt)" env:"TLS_SERVER_CERTS_DIR"`
+	Network            string `default:"tcp"                                                                                        help:"Network on which to listen for gRPC connections."`
+	Address            string `default:":9443"                                                                                      help:"Address at which to listen for gRPC connections."`
+	TLSCertsDir        string `env:"TLS_SERVER_CERTS_DIR"                                                                           help:"Directory containing server certs (tls.key, tls.crt) and the CA used to verify client certificates (ca.crt)"`
 	Insecure           bool   `help:"Run without mTLS credentials. If you supply this flag --tls-server-certs-dir will be ignored."`
-	MaxRecvMessageSize int    `help:"Maximum size of received messages in MB." default:"4"`
+	MaxRecvMessageSize int    `default:"4"                                                                                          help:"Maximum size of received messages in MB."`
 }
 
 // Run this Function.
